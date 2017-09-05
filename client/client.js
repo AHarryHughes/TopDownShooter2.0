00369ae887f33b1f4f96a26df42488b10352f9a8
AHarryHughes <Ahhughes8@gmail.com>
2017-09-05 07:11:49 -0400

    before I break it with socket

--- client/client.js
+++ client/client.js
@@ -1,2 +1,17 @@
 var client = {};
 client.socket = io.connect();
+
+//Socket.io should set the gameSave as this if there is not any gameSave on the user and load one if there is
+//And these should be set as method to the gameSave object
+
+client.save = function(saveState){
+    Client.socket.emit('save', saveState);
+};
+
+client.load = function(){
+    Client.socket.emit('runLoad');
+};
+
+Client.socket.on('load',function(data){
+    game.saveState = data;
+});
--- client/game.js
+++ client/game.js
@@ -6,24 +6,22 @@ game.state.add('levelOutside', LevelOutside);
 game.state.add('levelHouse', LevelHouse);
 game.state.start('Boot');
 
-// var gameSave = {
-//
-//     towers: [],
-//
-//     mercs: [],
-//
-//     wave: 0,
-//
-//     player: {},
-//
-//     weapons: [],
-//
-//     save: ,
-//
-//     load:
-//
-// };
+game.saveState = {
+
+    towers: null,
+
+    mercs: null,
+
+    wave: null,
+
+    lvl: null,
+
+    XP: null,
+
+    weapons: null,
+
+    money: null
+
+};
 
 
-//Socket.io should set the gameSave as this if there is not any gameSave on the user and load one if there is
-//And these should be set as method to the gameSave object
